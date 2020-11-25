cce3dd8a4be0d5bbdc2bef9658810a5044007a6c
ridhoajibx <ridhoajibx@gmail.com>
2020-11-26 04:55:04 +0700

    [ridho]-create home & form input components

---
+++ src/components/home/Form.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import useInput from '../../customhooks/useInput';
+
+const Form = () => {
+    const [title, bindTitle, resetTitle] = useInput();
+    const [content, bindContent, resetContent] = useInput();
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log({title, content})
+        resetTitle()
+        resetContent()
+    }
+    return (
+        <div className="section">
+        <form onSubmit={ handleSubmit } className="white p-4">
+                <h5 className="grey-text">New Note</h5>
+                <div className="input-field">
+                    <input id="note_title" type="text" className="validate" {...bindTitle} />
+                    <label className="active" htmlFor="note_title">Note title</label>
+                </div>
+                <div className="input-field">
+                    <textarea id="note_content" className="materialize-textarea" {...bindContent}></textarea>
+                    <label htmlFor="note_content">Note content</label>
+                </div>
+                <button className="btn green">Add Note</button>
+            </form>
+        </div>
+    );
+}
+
+export default Form;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Form from './Form';
 
 const Home = () => {
     return (
         <div className="container">
-            This is home
+            <div className="row center-align">
+                <div className="col s12 m7"><Form /></div>
+                <div className="col s12 m5">Note list</div>
+            </div>
         </div>
     );
 }
