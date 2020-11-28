b8570b48a6d6089180f876500bca3b026f3a0e38
ridhoajibx <ridhoajibx@gmail.com>
2020-11-28 13:59:48 +0700

    [ridho]-added moment js to display createdAt

--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { deleteNote, toggleFav } from '../../store/actions/noteAction';
+import moment from 'moment'
 
 const Note = ({ note }) => {
     const dispatch = useDispatch()
@@ -19,7 +20,7 @@ const Note = ({ note }) => {
             </div>
             <h5 className='black-text'>{note.title}</h5>
             <p className="truncate">{note.content}</p>
-            <p className="grey-text">2 days ago</p>
+            <p className="grey-text">{moment(note.createdAt.toDate()).fromNow()}</p>
             <div className="right-align">
                 <i className="material-icons" style={{ cursor: 'pointer' }}>edit</i>
             </div>
