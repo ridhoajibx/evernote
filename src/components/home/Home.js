446223c305f4170415d37489b24588befd660630
ridhoajibx <ridhoajibx@gmail.com>
2020-11-28 12:39:04 +0700

    [ridho]-create notelist component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect } from 'react-redux-firebase';
+import NotesList from '../notes/NotesList';
 import Form from './Form';
 
 const Home = () => {
+    useFirestoreConnect([
+        {
+            collection: 'notes',
+            orderBy: ['createdAt', 'desc']
+        }
+    ])
+    const notes = useSelector(state => state.firestore.ordered.notes)
+    console.log(notes);
     return (
         <div className="container">
             <div className="row center-align">
                 <div className="col s12 m7"><Form /></div>
-                <div className="col s12 m5">Note list</div>
+                <div className="col s12 m5"><NotesList /></div>
             </div>
         </div>
     );
